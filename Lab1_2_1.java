97b931f9bae469c3a33d60230c30ba6b232c42ce
kovarom <kovarom@gmail.com>
2017-03-12 23:24:41 +0200

    Add files via upload

---
+++ Lab1_2_1.java
@@ -0,0 +1,22 @@
+
+package lab1_2_1;
+
+
+public class Lab1_2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("  *******      *   *             *");
+        System.out.println("        *     * *   *           *");
+        System.out.println("        *    *   *   *         *");
+        System.out.println("        *   *     *   *       *");
+        System.out.println("        *   *******    *     *");
+        System.out.println(" *      *   *     *     *   *");
+        System.out.println("  *     *   *     *      * *");
+        System.out.println("   ******   *     *       *");
+        
+    }
+    
+}
